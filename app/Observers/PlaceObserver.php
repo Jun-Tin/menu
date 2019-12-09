e4651f7353d7ef106d085a91fc511c8e20921110
Jun-Tin <262323675@qq.com>
2019-12-09 12:18:12 +0800

    fix

--- app/Observers/PlaceObserver.php
+++ app/Observers/PlaceObserver.php
@@ -7,14 +7,14 @@ class PlaceObserver
 {
 	public function saving(Place $place)
 	{
-		$last = Place::where('store_id', $place->store_id)->where('floor', '<>', 0)->orderBy('id', 'desc')->first();
-		dd($last);
-		$name = Place::where('store_id', $place->store_id)->where('floor', '<>', 0)->value('name')?Place::where('store_id', $place->store_id)->where('floor', '<>', 0)->value('name')+1:'001';
-		dd($name);
-		$place->update([
-			'name' => '001',
-			'number' => 1,
-			'image_id' => 0,
-		]);
+		// $last = Place::where('store_id', $place->store_id)->where('floor', '<>', 0)->orderBy('id', 'desc')->first();
+		// dd($last);
+		// $name = Place::where('store_id', $place->store_id)->where('floor', '<>', 0)->value('name')?Place::where('store_id', $place->store_id)->where('floor', '<>', 0)->value('name')+1:'001';
+		// dd($name);
+		// $place->update([
+		// 	'name' => '001',
+		// 	'number' => 1,
+		// 	'image_id' => 0,
+		// ]);
 	}
 } 
