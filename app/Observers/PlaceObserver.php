81c572ca9d3d62eefe6f974e96ffdc668ccc91fb
Jun-Tin <262323675@qq.com>
2019-12-09 12:36:33 +0800

    fix

--- app/Observers/PlaceObserver.php
+++ app/Observers/PlaceObserver.php
@@ -5,16 +5,12 @@ use App\Models\Place;
 
 class PlaceObserver
 {
-	public function saving(Place $place)
+	public function created(Place $place)
 	{
-		// $last = Place::where('store_id', $place->store_id)->where('floor', '<>', 0)->orderBy('id', 'desc')->first();
-		// dd($last);
-		// $name = Place::where('store_id', $place->store_id)->where('floor', '<>', 0)->value('name')?Place::where('store_id', $place->store_id)->where('floor', '<>', 0)->value('name')+1:'001';
-		// dd($name);
-		// $place->update([
-		// 	'name' => '001',
-		// 	'number' => 1,
-		// 	'image_id' => 0,
-		// ]);
+		$place->update([
+			'name' => '新座位'.$place->id,
+			'number' => 1,
+			'image_id' => 0,
+		]);
 	}
 } 
