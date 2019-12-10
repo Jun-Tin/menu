6854c55324beba5502a1609eb9e0de3680b1d824
Jun-Tin <262323675@qq.com>
2019-12-10 10:32:57 +0800

    fix

--- app/Observers/PlaceObserver.php
+++ app/Observers/PlaceObserver.php
@@ -7,10 +7,12 @@ class PlaceObserver
 {
 	public function created(Place $place)
 	{
-		$place->update([
-			'name' => '新座位'.$place->id,
-			'number' => 1,
-			'image_id' => 0,
-		]);
+		if ($place->pid != 0) {
+			$place->update([
+				'name' => '新座位'.$place->id,
+				'number' => 1,
+				'image_id' => 0,
+			]);
+		}
 	}
 } 
