a8c523916b9f0e0468bb7925c3077a967c7e4aa2
Jun-Tin <262323675@qq.com>
2019-12-10 13:00:35 +0800

    fix

--- app/Observers/PlaceObserver.php
+++ app/Observers/PlaceObserver.php
@@ -1,7 +1,7 @@
 <?php 
 namespace App\Observers;
 
-use App\Models\Place;
+use App\Models\{Place, Image};
 use Illuminate\Support\Facades\{Auth, Storage, File, Crypt};
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
