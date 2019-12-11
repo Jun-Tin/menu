0351870a8a5ab22c9a38afe4ef6f3b05c8286a4d
Jun-Tin <262323675@qq.com>
2019-12-11 12:00:38 +0800

    fix

--- app/Http/Controllers/Api/ShopcartsController.php
+++ app/Http/Controllers/Api/ShopcartsController.php
@@ -199,9 +199,11 @@ class ShopcartsController extends Controller
             $item->tags = Tag::where('pid', $item->id)->get()->first();
             return $item->only('tags');
         })->values();
+
         $colletion->map(function ($item) use ($shopcart){
             $shopcart->tags_id .= $item['tags']['id'].',';
         });
+        dd($shopcart);
         $shopcart->tags_id = '[['.substr($shopcart->tags_id, 0, -1).']]';
         $shopcart->original_price = $menu_price;
         $shopcart->price = $menu_price;
