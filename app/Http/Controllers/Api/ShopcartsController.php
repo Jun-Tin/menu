9a11afabfed7caaba313bbff88046219313a66ed
Jun-Tin <262323675@qq.com>
2019-12-11 12:03:29 +0800

    fix

--- app/Http/Controllers/Api/ShopcartsController.php
+++ app/Http/Controllers/Api/ShopcartsController.php
@@ -196,10 +196,9 @@ class ShopcartsController extends Controller
         $shopcart->fill($request->all());
         $shopcart->number = 1;
         $colletion = $menu->tags()->where('category', 'perfer')->get()->filter(function ($item){
-            $item->tags = Tag::where('pid', $item->id)->get()->first();
+            $item->tags = Tag::where('pid', $item->id)->get();
             return $item->only('tags');
         })->values();
-        dd($colletion);
         $colletion->map(function ($item) use ($shopcart){
             $shopcart->tags_id .= $item['tags']['id'].',';
         });
