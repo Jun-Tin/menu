672255904986ffb27bdd21ba9f349d0e192b7acc
Jun-Tin <262323675@qq.com>
2019-12-12 15:09:41 +0800

    fix

--- app/Http/Controllers/Api/PlacesController.php
+++ app/Http/Controllers/Api/PlacesController.php
@@ -20,7 +20,7 @@ class PlacesController extends Controller
      */
     public function store(Request $request, Place $place, Image $image)
     {
-        dd(Redis::get('新座位88_88'));
+        dd(Redis::get('新座位103_103'));
         $place->fill($request->all());
         $place->status = 0;
         $place->save();
