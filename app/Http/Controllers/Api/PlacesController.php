921073184ccef06ffe080db7e5009ff60baa3156
Jun-Tin <262323675@qq.com>
2019-12-13 11:17:32 +0800

    fix

--- app/Http/Controllers/Api/PlacesController.php
+++ app/Http/Controllers/Api/PlacesController.php
@@ -22,7 +22,7 @@ class PlacesController extends Controller
     {
         // eyJpdiI6ImFZXC9
         // eyJpdiI6Ik5yWW1
-        dd(Redis::get('新座位108_108'));
+        dd(Redis::get('新座位*'));
         $place->fill($request->all());
         $place->status = 0;
         $place->save();
