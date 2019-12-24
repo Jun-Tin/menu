88bb8c981471990d36382a9262a5f88a7c2091fb
Jun-Tin <262323675@qq.com>
2019-12-24 16:26:50 +0800

    fix

--- app/Observers/BehaviorObserver.php
+++ app/Observers/BehaviorObserver.php
@@ -11,6 +11,7 @@ class BehaviorObserver
 		switch ($behavior->category) {
 			// 上菜
 			case 'serving':
+				dd(123123123);
 				$order_detail = $behavior->order_detail;
 				dd($order_detail);
 				// 修改菜品状态
