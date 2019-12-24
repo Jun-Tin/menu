54e982eb2aa0bf4ce03131bd1f8ddb27d1c0b0c6
Jun-Tin <262323675@qq.com>
2019-12-24 18:00:28 +0800

    fix

--- app/Observers/BehaviorObserver.php
+++ app/Observers/BehaviorObserver.php
@@ -19,7 +19,7 @@ class BehaviorObserver
 					// 获取套餐内单品状态
 					$all = OrderDetail::where('pid', $order_detail->pid)->select('status')->get();
 					$status = $all->containsStrict(function ($value, $key) {
-						dump($value['value']['status']);
+						dd($value['value']);
 					    return $value['value'] = 4;
 					});
 					dd($status);
