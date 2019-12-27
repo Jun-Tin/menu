4a4b62be6a5a9a28187bf713b6f261f5ec6131bc
Jun-Tin <262323675@qq.com>
2019-12-27 15:58:32 +0800

    fix

--- app/Http/Controllers/Api/AreasController.php
+++ app/Http/Controllers/Api/AreasController.php
@@ -14,7 +14,7 @@ class AreasController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Area $area)
     {
         return (new AreaResource($area))->additional(['status' => 200]);
     }
