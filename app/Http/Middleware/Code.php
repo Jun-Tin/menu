0cbfb389b34f18ae23648ea5d5fdeed70a33448e
Jun-Tin <262323675@qq.com>
2020-01-03 17:13:27 +0800

    fix

--- app/Http/Middleware/Code.php
+++ app/Http/Middleware/Code.php
@@ -48,7 +48,7 @@ class Code
                 $id = $store->id;
                 $store_id = $store->id;
             }
-            dd(Redis::get('新座位_64_64'), 0, 20);
+            dd(Redis::get('新座位64_9_64'), 0, 20);
             // 执行动作，判断该值是否存在redis数组
             if (substr(Redis::get($name.'_'.$store_id.'_'.$id), 0, 20) != $request->header('code')) {
                 return response()->json(['error' => ['message' => ['非法访问！']], 'status' => 404]);
