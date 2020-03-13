b219e2c129c28a5ee3933e300fdafec17f180705
Jun-Tin <262323675@qq.com>
2020-03-13 18:17:45 +0800

    fix

--- database/migrations/2020_03_13_181007_add_name_en_to_menus_table.php
+++ database/migrations/2020_03_13_181007_add_name_en_to_menus_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddCurrencyIdToStoresTable extends Migration
+class AddNameEnToMenusTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,8 @@ class AddCurrencyIdToStoresTable extends Migration
      */
     public function up()
     {
-        Schema::table('stores', function (Blueprint $table) {
-            $table->integer('currency_id')->nullable()->after('language_id')->comment('货币id');
+        Schema::table('menus', function (Blueprint $table) {
+            $table->string('name_en')->nullable()->after('name')->comment('英文名称');
         });
     }
 
@@ -25,8 +25,8 @@ class AddCurrencyIdToStoresTable extends Migration
      */
     public function down()
     {
-        Schema::table('stores', function (Blueprint $table) {
-            $table->dropColumn('currency_id');
+        Schema::table('menus', function (Blueprint $table) {
+            $table->dropColumn('name_en');
         });
     }
 }
