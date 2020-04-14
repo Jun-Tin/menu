dcbfa0dd7e4cbaa7d11ad6fa316f9b14fe66842c
Jun-Tin <262323675@qq.com>
2020-04-14 14:23:55 +0800

    fix

--- app/Observers/OrderDetailObserver.php
+++ app/Observers/OrderDetailObserver.php
@@ -18,7 +18,7 @@ class OrderDetailObserver
 				}
 				$remark = $string. $orderdetail->remark;
 			}
-			Gateway::sendToGroup('chef_'.$orderdetail->store_id, json_encode(array('type' => 'new_dishes', 'message' => '新菜品通知！', 'place_name' => $orderdetail->place->name, 'menu_name' => $orderdetail->pid? $orderdetail->packageMenu->name: $orderdetail->menu->name, 'menu_enname' => $orderdetail->pid? $orderdetail->packageMenu->name_en: $orderdetail->menu->name_en, 'remark' => $remark, 'created_at' => $orderdetail->created_at->format('Y/m/d H:i:s')), JSON_UNESCAPED_UNICODE));
+			Gateway::sendToGroup('chef_'.$orderdetail->store_id, json_encode(array('type' => 'new_dishes', 'message' => '新菜品通知！', 'place_name' => $orderdetail->place->name, 'menu_name' => $orderdetail->pid? $orderdetail->packageMenu->name: $orderdetail->menu->name, 'menu_name_en' => $orderdetail->pid? $orderdetail->packageMenu->name_en: $orderdetail->menu->name_en, 'remark' => $remark, 'created_at' => $orderdetail->created_at->format('Y/m/d H:i:s')), JSON_UNESCAPED_UNICODE));
 		}
 	}
 } 
